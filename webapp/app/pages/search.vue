87c987b93a5829010403bf63b17d93fb8320604f
yuraigle <yuraigle@gmail.com>
2025-08-27 08:35:57 +0800

    ...

---
+++ webapp/app/pages/search.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const router = useRouter()
+const q = computed(() => router.currentRoute.value.query.q)
+</script>
+
+<template>
+  <div>
+    <h1 class="text-2xl font-bold py-2">Search Results</h1>
+    <p>{{ q }}</p>
+  </div>
+</template>
