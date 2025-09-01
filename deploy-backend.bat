66379d5b7188b13d08bfeeec378f9a9bb77bb308
yuraigle <yuraigle@gmail.com>
2025-09-01 20:17:57 +0800

    deploy scripts

---
+++ deploy-backend.bat
@@ -0,0 +1,7 @@
+cd /D %~dp0
+call mvnw clean package
+
+pscp -i C:/Users/yurai/.ssh/id_sber1.ppk .\target\adrift.jar user1@176.108.244.255:/home/user1/adrift/
+
+ssh user1@176.108.244.255 -i C:/Users/yurai/.ssh/id_sber1 -t ^
+  "bash -i -c 'sudo systemctl restart adrift && echo OK'"
--- deploy/deploy-backend.bat
+++
@@ -1,7 +0,0 @@
-cd /D %~dp0\..
-call mvnw clean package
-
-cd /D %~dp0
-winscp.com /script=./winscp-backend.txt
-
-ssh user1@176.108.244.255 -i C:/Users/yurai/.ssh/id_sber1 -t "bash -i -c 'sudo systemctl restart adrift && echo OK'"
