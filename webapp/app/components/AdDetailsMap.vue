e1678258f1d27f91e6ee32b6e3d197013147781c
orlov <orlov@irkoms.ru>
2025-09-09 12:15:22 +0800

    interactive map

--- webapp/app/components/AdDetailsMap.vue
+++ webapp/app/components/AdDetailsMap.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
+import mapboxgl from 'mapbox-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
+
 const props = defineProps<{ lat: number, lon: number }>()
 
-const zoom = 14
+const zoom: number = 12
 const w: number = 760
 const h: number = Math.round(w / 16 * 9)
 const token: string = useAppConfig().MAPBOX_TOKEN
@@ -16,13 +19,29 @@ const mapPic = computed((): string => {
     `/pin-l+0092b8(${lon},${lat})/${lon},${lat},${zoom},0/${w}x${h}${x2}` +
     `?access_token=${token}`
 })
+
+const onClickMap = () => {
+  const map: mapboxgl.Map = new mapboxgl.Map({
+    accessToken: token,
+    container: 'map1',
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [props.lon, props.lat],
+    zoom: zoom
+  });
+
+  new mapboxgl.Marker({ color: '#0092b8' })
+    .setLngLat([props.lon, props.lat])
+    .addTo(map);
+}
 </script>
 
 <template>
   <div class="mt-2 w-full aspect-[16/9]">
     <div
-      class="w-full h-full bg-center bg-no-repeat"
+      id="map1"
+      class="w-full h-full bg-center bg-no-repeat bg-cover rounded-md cursor-pointer"
       :style="{ 'background-image': `url('${mapPic}')` }"
+      @click="onClickMap"
     />
   </div>
 </template>
